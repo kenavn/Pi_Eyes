db890a0ddd208e8cf7fd2056809e0621e9ee0361
Kenneth Avner <kenneth@avner.no>
2024-10-25 00:15:46 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,14 @@
 
 # Define the source and destination paths
 source_path="/home/kenneth/Source/Pi_Eyes/eyes.py"
-destination_path="kenneth@raspieyes:eyes.py"
+destination_path1="kenneth@raspieyes:eyes.py"
+destination_path2="kenneth@raspieyes2:eyes.py"
+
 
 # Copy the file using scp
 scp "$source_path" "$destination_path"
+scp "$source_path" "$destination_path2"
 
 # Connect via SSH and move the file using sudo
 ssh kenneth@raspieyes "sudo mv /home/kenneth/eyes.py /boot/Pi_Eyes/eyes.py && sudo reboot now"
+ssh kenneth@raspieyes2 "sudo mv /home/kenneth/eyes.py /boot/Pi_Eyes/eyes.py && sudo reboot now"
